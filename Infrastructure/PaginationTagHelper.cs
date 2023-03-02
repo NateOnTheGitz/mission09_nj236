f7c83ad06af837bd4bf32b814cb8d1255cacaa33
Nathan Johnson <nj236@byu.edu>
2023-03-02 01:30:00 -0700

    finalize styling

--- Infrastructure/PaginationTagHelper.cs
+++ Infrastructure/PaginationTagHelper.cs
@@ -30,6 +30,12 @@ namespace mission09_nj236.Infrastructure
         public PageInfo PageBlah { get; set; }
         public string PageAction { get; set; }
 
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set; }
+
+
         public override void Process (TagHelperContext thc, TagHelperOutput tho)
         {
             IUrlHelper uh = uhf.GetUrlHelper(vc);
@@ -41,6 +47,11 @@ namespace mission09_nj236.Infrastructure
                 TagBuilder tb = new TagBuilder("a");
 
                 tb.Attributes["href"] = uh.Action(PageAction, new { pageNum = i });
+                if (PageClassesEnabled)
+                {
+                    tb.AddCssClass(PageClass);
+                    tb.AddCssClass(i == PageBlah.CurrentPage ? PageClassSelected : PageClassNormal);
+                }
                 tb.InnerHtml.Append(i.ToString());
 
                 final.InnerHtml.AppendHtml(tb);

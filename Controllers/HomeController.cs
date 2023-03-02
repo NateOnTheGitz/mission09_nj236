94d92d3714324ec47d87b2a8a8d2fd1829fd27fd
Nathan Johnson <nj236@byu.edu>
2023-03-01 23:57:02 -0700

    working page navigation

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using mission09_nj236.Models;
+using mission09_nj236.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,10 +14,25 @@ namespace mission09_nj236.Controllers
 
         public HomeController(IBookstoreRepository temp) => repo = temp;
 
-        public IActionResult Index()
+        public IActionResult Index(int pageNum = 1)
         {
-            var data = repo.Books.ToList();
-            return View(data);
+            int pageSize = 10;
+
+            var x = new BooksViewModel
+            {
+                Books = repo.Books
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumBooks = repo.Books.Count(),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+
+            return View(x);
         }
     }
 }

78b655521c1c74a3feb3195276e9ab2a635f2e44
Nathan Johnson <nj236@byu.edu>
2023-03-01 20:00:52 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mission09_nj236.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mission09_nj236.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBookstoreRepository repo;
+
+        public HomeController(IBookstoreRepository temp) => repo = temp;
+
+        public IActionResult Index()
+        {
+            var data = repo.Books.ToList();
+            return View(data);
+        }
+    }
+}

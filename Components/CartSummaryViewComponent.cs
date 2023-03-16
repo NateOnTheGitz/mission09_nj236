b8fa628218dc8dc52ff865c29bd0c43f491d571f
Nathan Johnson <nj236@byu.edu>
2023-03-15 23:32:25 -0600

    Cart Icon

---
+++ Components/CartSummaryViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using mission09_nj236.Models;
+
+namespace mission09_nj236.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Basket basket;
+        public CartSummaryViewComponent (Basket bservice)
+        {
+            basket = bservice;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(basket);
+        }
+    }
+}
